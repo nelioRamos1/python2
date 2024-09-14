442b1d905bde9964d5b9644bbcd6dd1c1112fe5f
unknown <nelioramos642@gmail.com>
2024-09-14 10:47:10 -0300

    oi

---
+++ git22/funções.py
@@ -0,0 +1,11 @@
+import string
+
+def soma (a, b):
+    return a + b
+
+
+num1 = float(input("digite o primeiro numero:"))
+num2 = float(input("digite o segundo numero:"))
+
+resultado = soma(num1, num2)
+print(resultado)
---
+++ git22/lista.py
@@ -0,0 +1,4 @@
+frutas = ["maca", "pera", "uva", "banana"]
+
+for fruta in frutas:
+    print(fruta)
